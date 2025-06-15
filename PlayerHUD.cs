e41c5722c1635b52d137f49dd389383650666153
ArghBeef <167025940+ArghBeef@users.noreply.github.com>
2025-06-16 01:11:11 +0200

    Update PlayerHUD.cs

--- PlayerHUD.cs
+++ PlayerHUD.cs
@@ -1,16 +1,16 @@
-﻿using System.Collections;
-using UnityEngine;
+using System.Collections;
 using Unity.Netcode;
-using UnityEngine.UI;
 using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHUD : MonoBehaviour
 {
-    public Slider hpBar;
-    public TMP_Text hpText;
-    public TMP_Text weaponNameText;
-    public TMP_Text ammoText;
-    public Slider reloadBar;
+    [SerializeField] Slider hpBar;
+    [SerializeField] TMP_Text hpText;
+    [SerializeField] TMP_Text weaponNameText;
+    [SerializeField] TMP_Text ammoText;
+    [SerializeField] Slider reloadBar;
 
     Player player;
 
@@ -19,8 +19,7 @@ public class PlayerHUD : MonoBehaviour
         while (player == null)
         {
             if (NetworkManager.Singleton.LocalClient?.PlayerObject != null)
-                player = NetworkManager.Singleton
-                                   .LocalClient.PlayerObject.GetComponent<Player>();
+                player = NetworkManager.Singleton.LocalClient.PlayerObject.GetComponent<Player>();
             yield return null;
         }
 
@@ -29,10 +28,10 @@ public class PlayerHUD : MonoBehaviour
         hpBar.value = hp.current.Value;
         hpText.text = hp.current.Value.ToString("0");
 
-        hp.current.OnValueChanged += (oldVal, newVal) =>
+        hp.current.OnValueChanged += (_, val) =>
         {
-            hpBar.value = newVal;
-            hpText.text = newVal.ToString("0");
+            hpBar.value = val;
+            hpText.text = val.ToString("0");
         };
 
         reloadBar.gameObject.SetActive(false);
@@ -51,15 +50,13 @@ public class PlayerHUD : MonoBehaviour
         }
 
         var w = player.CurrentWeapon;
-
-        weaponNameText.text = w.data.weaponName.ToUpper();
-        ammoText.text = $"{w.CurrentAmmo} / {w.data.magazineSize}";
+        weaponNameText.text = w.Data.weaponName.ToUpper();
+        ammoText.text = $"{w.CurrentAmmo} / {w.Data.magazineSize}";
 
         if (w.IsReloading)
         {
             if (!reloadBar.gameObject.activeSelf)
                 reloadBar.gameObject.SetActive(true);
-
             reloadBar.value = w.ReloadFraction;
         }
         else if (reloadBar.gameObject.activeSelf)

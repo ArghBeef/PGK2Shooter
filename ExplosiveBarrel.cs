8840d6860c55be606608121a1b2da34ecc09af75
ArghBeef <167025940+ArghBeef@users.noreply.github.com>
2025-06-16 01:10:56 +0200

    Update ExplosiveBarrel.cs

--- ExplosiveBarrel.cs
+++ ExplosiveBarrel.cs
@@ -1,47 +1,80 @@
-﻿using UnityEngine;
 using Unity.Netcode;
+using UnityEngine;
+using System.Collections;
 
 [RequireComponent(typeof(Health), typeof(NetworkObject))]
 public class ExplosiveBarrel : NetworkBehaviour
 {
-    [Header("Physics")]
-    public float explosionRadius = 5f;
-    public float explosionForce = 700f;
-    public float upwardModifier = 1f;
+    [SerializeField] float explosionRadius = 5f;
+    [SerializeField] float explosionForce = 700f;
+    [SerializeField] float upwardModifier = 1f;
+    [SerializeField] GameObject explosionEffectPrefab;
+    [SerializeField] float explosionDamage = 60f;
 
+    Renderer[] renderers;
+    Collider[] colliders;
 
-    public GameObject explosionEffectPrefab;
-    public float explosionDamage = 60f;
-
-    void Awake() =>
+    void Awake()
+    {
         GetComponent<Health>().onDeath.AddListener(Explode);
+        renderers = GetComponentsInChildren<Renderer>();
+        colliders = GetComponentsInChildren<Collider>();
+    }
 
     void Explode()
     {
         if (!IsServer) return;
 
-        if (explosionEffectPrefab)
-            Destroy(Instantiate(explosionEffectPrefab,
-                                 transform.position,
-                                 Quaternion.identity), 3f);
+        SpawnExplosionClientRpc(transform.position);
 
         foreach (Collider hit in Physics.OverlapSphere(transform.position, explosionRadius))
         {
-            var rb = hit.attachedRigidbody;
-            if (rb)
-                rb.AddExplosionForce(explosionForce, transform.position,
-                                     explosionRadius, upwardModifier,
-                                     ForceMode.Impulse);
-
-            var hp = hit.GetComponent<Health>();
-            if (hp)
+            if (hit.TryGetComponent(out Health hp) && hp != GetComponent<Health>())
             {
-                float dist = Vector3.Distance(transform.position, hit.transform.position);
-                float frac = Mathf.InverseLerp(explosionRadius, 0f, dist);
+                float frac = Mathf.InverseLerp(explosionRadius, 0f,
+                                               Vector3.Distance(transform.position, hit.transform.position));
                 hp.TakeDamageServerRpc(explosionDamage * frac);
+            }
+
+            if (hit.attachedRigidbody != null)
+            {
+                hit.attachedRigidbody.AddExplosionForce(
+                    explosionForce,
+                    transform.position,
+                    explosionRadius,
+                    upwardModifier,
+                    ForceMode.Impulse
+                );
             }
         }
 
-        NetworkObject.Despawn();
+        HideBarrelClientRpc();
+        StartCoroutine(DelayedDespawn());
+    }
+
+    IEnumerator DelayedDespawn()
+    {
+        yield return new WaitForSeconds(0.05f);
+        NetworkObject.Despawn(true);
+    }
+
+    [ClientRpc]
+    void SpawnExplosionClientRpc(Vector3 pos)
+    {
+        if (explosionEffectPrefab != null)
+        {
+            var fx = Instantiate(explosionEffectPrefab, pos, Quaternion.identity);
+            Destroy(fx, 3f);
+        }
+    }
+
+    [ClientRpc]
+    void HideBarrelClientRpc()
+    {
+        foreach (var r in renderers)
+            r.enabled = false;
+
+        foreach (var c in colliders)
+            c.enabled = false;
     }
 }
